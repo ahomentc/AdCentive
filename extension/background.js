263ac4cc6967ab828dbc5c3c050355f189896e01
Sriram Sundar <sriramsundar@vmware.com>
2018-11-11 02:37:57 -0800

    change extension

--- extension/background.js
+++ extension/background.js
@@ -10,16 +10,18 @@
 /*****************************************************************
  * onMessage from the extension or tab (a content script)
  *****************************************************************/
-/*chrome.runtime.onMessage.addListener(
+chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if (request.message == "Click") {
         if (localStorage.getItem("total_elements") === null) {
             console.log("first");
             localStorage["total_elements"] = 1; // Get from EOS backend
+            window.val = localStorage["total_elements"];
           }
         else {
             console.log("second");
             localStorage["total_elements"] =  Number(localStorage["total_elements"]) + Number(1);
+            window.val = localStorage["total_elements"];
         }
         sendResponse({ farewell: "any response from background" });
       } else {
@@ -29,4 +31,3 @@
       //  ref: http://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
       return true; 
     });
-*/
--- extension/popup.js
+++ extension/popup.js
@@ -8,25 +8,8 @@
 	});
 });
 */
-console.log("test: ", localStorage["total_elements"]);
-document.getElementById("tokens").value = localStorage["total_elements"];
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (request.message == "Click") {
-        if (localStorage.getItem("total_elements") === null) {
-            console.log("first");
-            localStorage["total_elements"] = 1; // Get from EOS backend
-          }
-        else {
-            console.log("second");
-            localStorage["total_elements"] =  Number(localStorage["total_elements"]) + Number(1);
-        }
-        sendResponse({ farewell: "any response from background" });
-      } else {
-        sendResponse({ result: "error", message: `Invalid 'cmd'` });
-      }
-      // Note: Returning true is required here!
-      //  ref: http://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
-      return true; 
-    });
+function refresh() {
+console.log("test: ", localStorage["total_elements"]);
+document.getElementById("tokens").innerHTML = localStorage["total_elements"];
+}
