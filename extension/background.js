617df232bde4910068575eb21d24cbe60ab7dbd2
Sriram Sundar <sriramsundar@vmware.com>
2018-11-11 02:11:19 -0800

    Modifying extension

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,32 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+/*chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 	chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
         if (request.message == "Click") {
-            alert("clicked");
+            //alert("clicked");
         }
 	});
-});
+});*/
 
+/*****************************************************************
+ * onMessage from the extension or tab (a content script)
+ *****************************************************************/
+/*chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.message == "Click") {
+        if (localStorage.getItem("total_elements") === null) {
+            console.log("first");
+            localStorage["total_elements"] = 1; // Get from EOS backend
+          }
+        else {
+            console.log("second");
+            localStorage["total_elements"] =  Number(localStorage["total_elements"]) + Number(1);
+        }
+        sendResponse({ farewell: "any response from background" });
+      } else {
+        sendResponse({ result: "error", message: `Invalid 'cmd'` });
+      }
+      // Note: Returning true is required here!
+      //  ref: http://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
+      return true; 
+    });
+*/
--- extension/extension.js
+++ extension/extension.js
@@ -1,6 +1,10 @@
 document.addEventListener('click', (event) => {
 	console.log("ola2");
-	chrome.runtime.sendMessage({"message" : "Click"});
+	//chrome.runtime.sendMessage({"message" : "Click"});
+	chrome.runtime.sendMessage({message : "Click"}, function(response) {
+		console.log("resp: ", response.farewell);
+		chrome.runtime.sendMessage({message2 : "Click2"});
+	  });
 });
 
 /*var document = document.getElementById("iframe");
